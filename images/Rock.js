7cf6bfa1c9143f8b6ca2598801b63f7ad7d97926
PRINCE-CODER-100 <82875419+PRINCE-CODER-100@users.noreply.github.com>
2021-07-09 03:41:10 -0700

    Add files via upload

---
+++ images/Rock.js
@@ -0,0 +1,22 @@
+class Rock
+{
+    constructor(x, y, w, h)
+    {
+        var options = 
+        {
+            isStatic:false
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        this.image = loadImage("images/rock1.png");
+        World.add(myWorld, this.body);
+    }
+    show()
+    {
+        var pos = this.body.position;
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y, this.w, this.h);
+    }
+    
+}
